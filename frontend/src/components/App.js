8271db0621068684d44b11cf505e62852f65f811
Jesus Garcia <jgarc110@rams.colostate.edu>
2021-04-24 12:52:36 -0600

    We can now interact with the smart contract on the frontend :)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,37 +1,70 @@
 import React, { Component } from "react";
-
-import { SnackbarProvider, useSnackbar } from 'notistack';
-
-import Page from "./Page";
-import initBlockchain from "../utils/initBlockchain";
+import {chat} from "../utils/constants.js";
+import {ethers} from "ethers";
+import Web3 from "web3";
 
 
 export default class App extends Component {
 
 
     componentDidMount = async () => {
-        try {
-            const chatInfo = await initBlockchain(); // from utils directory;  connect to provider and to metamask or other signer
-        } catch (error) {
-            // Catch any errors for any of the above operations.
-            alert(`Failed to load provider, signer, or contract. Check console for details.`);
-            console.log(error);
-        }
+        this.loadBlockchainData();
     };
 
+
+    async loadBlockchainData(){
+
+        let provider;
+        window.ethereum.enable().then(provider = new ethers.providers.Web3Provider(window.ethereum));
+
+        const signer = provider.getSigner();
+
+        console.log("signer", signer);
+
+        let userAddress;
+        try {
+            userAddress =  await signer.getAddress();
+            console.log("user address", userAddress);
+        }catch (e) {
+            console.log(e)
+        }
+
+
+
+        let chatInstance = new ethers.Contract('0x5dAD9A7357A3ac3A1F7D3fb785939eBc3699BA21', chat.abi, signer);
+
+        this.setState({userAddress:userAddress, contract:chatInstance});
+
+        console.log("lesss gooo",chatInstance)
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userAccount: '',
+            toAccount: '',
+            msg: '',
+            contract: null,
+
+        }
+    }
+
+
+
     render() {
+
+
+        try{
+
+            this.state.contract.compose("0x58F57681d4C519Dbc30261d4cD080825A55c4380","OOF");
+
+
+        }catch (e) {
+            console.log(e)
+        }
         return (
-            <SnackbarProvider maxSnack={3} preventDuplicate>
-                <HookCaller />
-            </SnackbarProvider>
+            <h1>HI</h1>
         );
     }
 }
-
-export const HookCaller = () => {
-
-    const { enqueueSnackbar } = useSnackbar();
-    const createSnackBar = (message, variant="error") => enqueueSnackbar(message, { variant: variant });
-
-    return <Page createSnackBar={createSnackBar}/>;
-};
